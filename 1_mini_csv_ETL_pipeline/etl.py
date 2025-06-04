d3901f0e9d32ad5075590eda255c5c168e1a385d
Gabriel david MORENO REYES <gamoreno@f6r9s15.clusters.42paris.fr>
2025-06-04 15:10:26 +0200

    first miniproject ready

--- 1_mini_csv_ETL_pipeline/etl.py
+++ 1_mini_csv_ETL_pipeline/etl.py
@@ -5,13 +5,13 @@ import argparse
 
 def read_and_show_pure_data(path):
     df = pd.read_csv(path, )
+    print("\n------------------------------------------------------------------------\n")
     print(f" Pure data: \n {df}")
     print(f" Types of pure data readed:\n {df.dtypes}")
     return df
 
 def process_date_column(df):
     df['Date'] = pd.to_datetime(df['Date'])
-    df['Date'] = df['Date'].dt.strftime('%d-%m-%Y')
 
 def process_price_column(df):
     df['Price'] = df['Price'].str.replace(',', '').astype(float)
@@ -33,14 +33,22 @@ def process_change_column(df):
     df['Change %'] = df['Change %'].str.replace('%', '').astype(float) / 100
 
 def calculate_SMA(df):
-    df['sma_5'] = df['Price'].rolling(window=5).mean()
-    df['sma_20'] = df['Price'].rolling(window=20).mean()
+    df_sorted = df.sort_values(by='Date')
+    df_sorted['SMA_5'] = df_sorted['Price'].rolling(window=5).mean()
+    df_sorted['SMA_20'] = df_sorted['Price'].rolling(window=20).mean()
+    df.loc[df_sorted.index, 'SMA_5'] = df_sorted['SMA_5']
+    df.loc[df_sorted.index, 'SMA_20'] = df_sorted['SMA_20']
 
 def calculate_EMA(df):
-    df['EMA_5'] = df['Price'].ewm(span=5, adjust=False).mean()
-    df['EMA_20'] = df['Price'].ewm(span=20, adjust=False).mean()
+    df_sorted = df.sort_values(by='Date')
+    df_sorted['EMA_5'] = df_sorted['Price'].ewm(span=5, adjust=False).mean()
+    df_sorted['EMA_20'] = df_sorted['Price'].ewm(span=20, adjust=False).mean()
+    df.loc[df_sorted.index, 'EMA_5'] = df_sorted['EMA_5']
+    df.loc[df_sorted.index, 'EMA_20'] = df_sorted['EMA_20']
+
 
 def print_dataframe_data(df):
+    print("\n------------------------------------------------------------------------\n")
     print(f" Final dataFrame: \n {df}")
     print(f" Types of cleaned data: \n {df.dtypes}")
 
@@ -50,9 +58,12 @@ def print_dataframe_data(df):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='ETL pipeline for ETF data')
     parser.add_argument("data_csv_path", type=str, help="Path to the CSV file to read")
+    parser.add_argument("database_name", type=str, help="Name of the database to save")
     args = parser.parse_args()
-    path = args.data_csv_path
-    df = read_and_show_pure_data(path)
+    path_to_csv_file = args.data_csv_path
+    path_to_database_file = f"cleaned_data/{args.database_name}.db"
+    print("aca: ", path_to_database_file)
+    df = read_and_show_pure_data(path_to_csv_file)
 
     #------------------------------------------------------------------------
     # In these first functions, proper column types and formats are defined,
@@ -71,6 +82,7 @@ if __name__ == "__main__":
     # The following functions will calculate some valuable and interesting values
     # that will be stored in the database.
     #------------------------------------------------------------------------
+
     # SMA (Simple Moving Average) of the 5 and 20 last values:
     calculate_SMA(df)
 
@@ -78,20 +90,12 @@ if __name__ == "__main__":
     # Weighted Methods: ewn(), is used to give more weight to the recent prices
     # and then calculate the mean.
     calculate_EMA(df)
-
-
-
-
+    
     #------------------------------------------------------------------------
     # Now the final pandas dataframe is printed and the information
     # is stocked in a database
     #------------------------------------------------------------------------
     print_dataframe_data(df)
-    with sq.connect('cleansed_etf_data/cleaned_data.db') as conn:
+    with sq.connect(path_to_database_file) as conn:
         df.to_sql('etf_data', conn, if_exists='replace', index=False)
 
-
-    
-    # print(path)
-    
-    
